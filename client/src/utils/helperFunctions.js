50777a925edf2a39c044d85746073207d9179620
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2026-02-01 16:10:44 -0600

    add dupe removal in useDataFetchPaginated

--- client/src/utils/helperFunctions.js
+++ client/src/utils/helperFunctions.js
@@ -14,3 +14,27 @@ export function capitalizeFirstLetters(string) {
     .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
     .join(" ");
 }
+
+export function getDuplicatesRemovedMerged(...args) {
+  if (!args.every((arg) => Array.isArray(arg)))
+    throw new Error(
+      "non-Array argument passed to function 'getDuplicatesRemovedMerged'",
+    );
+
+  const idTracker = {};
+  const result = [];
+
+  const cb = (obj) => {
+    if (typeof obj !== "object" || !Object.hasOwn(obj, "id"))
+      return result.push(obj);
+
+    if (idTracker[obj.id]) return;
+
+    result.push(obj);
+    idTracker[obj.id] = true;
+  };
+
+  args.forEach((arg) => arg.forEach(cb));
+
+  return result;
+}
