d767df388b47bc1b4bbb19c465f59651fddd868a
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2026-02-12 23:47:10 -0600

    add styling to Login page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,9 @@
 import { useContext, useState } from "react";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 import api from "../../api";
 import AuthContext from "../../contexts/AuthContext";
 const apiHost = import.meta.env.VITE_API_HOST;
-// import styles from "./Login.module.css";
+import styles from "./Login.module.css";
 
 function SignUpForm() {
   const { setAuthFromResponse } = useContext(AuthContext);
@@ -47,55 +48,66 @@ function SignUpForm() {
     <>
       {isLoading && <p>Processing...</p>}
       {errors && (
-        <ul>
+        <ul className={styles.errorsList}>
           {errors.map((error, i) => (
             <li key={i}>{error.msg}</li>
           ))}
         </ul>
       )}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username: </label>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.textInput}>
+          <label htmlFor="username">Username</label>
           <input
             type="text"
             id="username"
+            placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            autoComplete="off"
             required
           />
         </div>
-        <div>
-          <label htmlFor="email">Email: </label>
+        <div className={styles.textInput}>
+          <label htmlFor="email">Email</label>
           <input
             type="email"
             id="email"
+            placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label htmlFor="password">Password: </label>
+        <div className={styles.textInput}>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
+            placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            autoComplete="new-password"
             required
           />
         </div>
-        <div>
-          <label htmlFor="passwordConfirmation">Confirm Password: </label>
+        <div className={styles.textInput}>
+          <label htmlFor="passwordConfirmation">Confirm password</label>
           <input
             type="password"
             id="passwordConfirmation"
+            placeholder="Confirm password"
             value={passwordConfirmation}
             onChange={(e) => setPasswordConfirmation(e.target.value)}
+            autoComplete="off"
             required
           />
         </div>
         <div>
-          <button type="submit" disabled={isLoading}>
+          <button
+            type="submit"
+            disabled={isLoading}
+            className={styles.signUpBtn}
+          >
             Sign Up
           </button>
         </div>
@@ -139,23 +151,25 @@ function LoginForm() {
     <>
       {isLoading && <p>Processing...</p>}
       {error && <p>{error}</p>}
-      <form onSubmit={handleLoginSubmit}>
-        <div>
-          <label htmlFor="username">Username or Email: </label>
+      <form onSubmit={handleLoginSubmit} className={styles.form}>
+        <div className={styles.textInput}>
+          <label htmlFor="username">Username or email</label>
           <input
             type="text"
             id="username"
+            placeholder="Username or email"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label htmlFor="password">Password: </label>
+        <div className={styles.textInput}>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
             value={password}
+            placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
@@ -169,11 +183,17 @@ function LoginForm() {
               setRememberDevice((prev) => !prev);
             }}
           />
-          <label htmlFor="rememberDevice">Remember Me</label>
+          <label htmlFor="rememberDevice" className={styles.rememberMeLabel}>
+            Remember Me
+          </label>
         </div>
         <div>
-          <button type="submit" disabled={isLoading}>
-            Login
+          <button
+            type="submit"
+            disabled={isLoading}
+            className={styles.logInBtn}
+          >
+            Log In
           </button>
         </div>
       </form>
@@ -186,7 +206,11 @@ function GoogleAuth() {
     window.location.href = `${apiHost}/auth/google`;
   };
 
-  return <button onClick={handleGoogleAuth}>Sign in with Google</button>;
+  return (
+    <button type="button" onClick={handleGoogleAuth}>
+      <FaGoogle />
+    </button>
+  );
 }
 
 function GithubAuth() {
@@ -194,7 +218,11 @@ function GithubAuth() {
     window.location.href = `${apiHost}/auth/github`;
   };
 
-  return <button onClick={handleGithubAuth}>Sign in with Github</button>;
+  return (
+    <button type="button" onClick={handleGithubAuth}>
+      <FaGithub />
+    </button>
+  );
 }
 
 function DemoLogin() {
@@ -223,8 +251,8 @@ function DemoLogin() {
 
   return (
     <>
-      <button onClick={handleDemoLogin} disabled={isLoading}>
-        Demo Account Login
+      <button type="button" onClick={handleDemoLogin} disabled={isLoading}>
+        Demo account
       </button>
       {error && <span>{error}</span>}
     </>
@@ -239,25 +267,55 @@ function Login() {
   };
 
   return (
-    <>
-      {showSignUp ? <SignUpForm /> : <LoginForm />}
-      {showSignUp ? (
-        <p>
-          Already have an account?<a onClick={toggleForm}>Log in</a>
-        </p>
-      ) : (
-        <a onClick={toggleForm}>Sign up for [insert site name]</a>
-      )}
-      <div>
-        <DemoLogin />
+    <div className={styles.primaryLayout}>
+      <div className={styles.hero}>
+        <div className={styles.title}>
+          <p>legally distinct social network app</p>
+          <h1>headlog</h1>
+        </div>
+        <h2 className={styles.flavorText}>
+          Waste your time and get addicted to endless content on Headlog.
+        </h2>
       </div>
-      <div>
-        <GoogleAuth />
+      <div className={styles.actionsContainer}>
+        {showSignUp ? (
+          <>
+            <SignUpForm />
+            <button
+              type="button"
+              onClick={toggleForm}
+              className={styles.toLoginFormBtn}
+            >
+              Already have an account?
+            </button>
+          </>
+        ) : (
+          <>
+            <LoginForm />
+            <div className={styles.divider}>
+              <span>Or continue with</span>
+            </div>
+            <div className={styles.alternateLoginsContainer}>
+              <div className={styles.oauthContainer}>
+                <GithubAuth />
+                <GoogleAuth />
+              </div>
+              <div className={styles.demoContainer}>
+                <DemoLogin />
+              </div>
+            </div>
+            <div className={styles.divider} />
+            <button
+              type="button"
+              onClick={toggleForm}
+              className={styles.newAccountBtn}
+            >
+              Create new account
+            </button>
+          </>
+        )}
       </div>
-      <div>
-        <GithubAuth />
-      </div>
-    </>
+    </div>
   );
 }
 
